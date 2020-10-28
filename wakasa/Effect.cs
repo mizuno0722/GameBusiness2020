b998f186c324372f9f027d4000533a01d013cd43
wakacaptain <57741956+wakacaptain@users.noreply.github.com>
2020-10-28 13:05:33 +0900

    エフェクト動いている時だけ表示

---
+++ wakasa/Effect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect : MonoBehaviour {
+
+    
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        //this.transform.rotation = new Vector4(0.0f, 0.0f, 0.0f,0.0f);
+            //Quaternion.Euler(0, 0, 0);
+    }
+}
