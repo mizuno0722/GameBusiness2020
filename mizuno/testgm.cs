d35bd501730de7132f614e248b6b195ba82248ca
MellMag <yda.tm.2360623@gmail.com>
2020-11-04 13:53:59 +0900

    [fix] 生成を修正

--- mizuno/testgm.cs
+++ mizuno/testgm.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class testgm : MonoBehaviour
 {
     StageData stageData;
+    int nowStageNum;
+    bool isMove;
 
     // Start is called before the first frame update
     void Start()
     {
+        nowStageNum = 0;
+        isMove = false;
         stageData = Resources.Load<StageData>("TestStageData");
-        Instantiate(stageData.stage[0]);
+        Instantiate(stageData.stage[nowStageNum]);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //debug
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isMove = true;
+
+        }
     }
 }
